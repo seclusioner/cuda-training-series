89541c9d6256279400f3136b76dfa9d599bda32a
seclusioner <fileuseingbyds@gmail.com>
2025-10-16 16:04:18 +0800

    update problem2 in hw2 & add some notes

--- exercises/hw2/matrix_mul_shared.cu
+++ exercises/hw2/matrix_mul_shared.cu
@@ -2,6 +2,8 @@
 
 // these are just for timing measurments
 #include <time.h>
+#include <cstdlib>  // rand()
+#include <ctime>    // time()
 
 // error checking macro
 #define cudaCheckErrors(msg) \
@@ -16,104 +18,146 @@
         } \
     } while (0)
 
-
-const int DSIZE = 8192;
+const int DSIZE = 4096;
 const int block_size = 32;  // CUDA maximum is 1024 *total* threads in block
-const float A_val = 3.0f;
-const float B_val = 2.0f;
+const float EPS = 1e-5;
 
 // matrix multiply (naive) kernel: C = A * B
-__global__ void mmul(const float *A, const float *B, float *C, int ds) {
-
-  // declare cache in shared memory
+// (M, N) = (M, K) @ (K, N)
+__global__ void mmul(const float *A, const float *B, float *C, int M, int K, int N) {
   __shared__ float As[block_size][block_size];
   __shared__ float Bs[block_size][block_size];
 
-  int idx = threadIdx.x+blockDim.x*blockIdx.x; // create thread x index
-  int idy = threadIdx.y+blockDim.y*blockIdx.y; // create thread y index
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-  if ((idx < ds) && (idy < ds)){
-    float temp = 0;
-    for (int i = 0; i < ds/block_size; i++) {
+  float temp = 0.0f;
 
-      // Load data into shared memory
-      As[threadIdx.y][threadIdx.x] = A[FIXME];
-      Bs[threadIdx.y][threadIdx.x] = B[FIXME];
+  for (int i = 0; i < (K + block_size - 1) / block_size; i++) {
+    int tiledCol = i * block_size + threadIdx.x;
+    int tiledRow = i * block_size + threadIdx.y;
 
-      // Synchronize
-      __syncthreads();
+    As[threadIdx.y][threadIdx.x] = (row < M && tiledCol < K) ? A[row * K + tiledCol] : 0.0f;
+    Bs[threadIdx.y][threadIdx.x] = (tiledRow < K && col < N) ? B[tiledRow * N + col] : 0.0f;
 
-      // Keep track of the running sum
-      for (int k = 0; k < block_size; k++)
-      	temp += As[FIXME][FIXME] * Bs[FIXME][FIXME]; // dot product of row and column
-      __syncthreads();
+    __syncthreads();
 
+    for (int k = 0; k < block_size; ++k) {
+      temp += As[threadIdx.y][k] * Bs[k][threadIdx.x];
     }
 
-    // Write to global memory
-    C[idy*ds+idx] = temp;
+    __syncthreads();
+  }
+
+  if (row < M && col < N) {
+    C[row * N + col] = temp;
   }
 }
 
-int main(){
+void mmul_host(const float *A, const float *B, float *C, int m, int n, int p){
+  for(int i=0;i<m;i++){
+    for(int j=0;j<p;j++){
+      for(int k=0;k<n;k++){
+        C[i*p+j] += A[i*n+k] * B[k*p+j];
+      }
+    }
+  }
+}
 
-  float *h_A, *h_B, *h_C, *d_A, *d_B, *d_C;
+int main(int argc, char* argv[]){
+  if (argc != 4) {
+      printf("Usage: %s <m> <n> <p>\n", argv[0]);
+      return 1;
+  }
+  srand(time(0));
 
+  int m = atoi(argv[1]);
+  int n = atoi(argv[2]);
+  int p = atoi(argv[3]);
+
+  if (m <= 0 || n <= 0 || p <= 0 || m > DSIZE || n > DSIZE || p > DSIZE) {
+      printf("Matrix sizes must be in range 1 to %d\n", DSIZE);
+      return 1;
+  }
+  printf("%d by %d multiply %d by %d\n", m, n, n, p);
+
+  float *h_A, *h_B, *h_C, *h_C2, *d_A, *d_B, *d_C;
 
   // these are just for timing
   clock_t t0, t1, t2;
   double t1sum=0.0;
   double t2sum=0.0;
 
+  // initialize
+  h_A = new float[m*n]; // m x n
+  h_B = new float[n*p]; // n x p
+  h_C = new float[m*p]; // m x p
+  h_C2 = new float[m*p];
+  for (int i = 0; i < (m*n); i++){
+    h_A[i] = (float)(rand() % 10);
+  }
+  for (int i = 0; i < (n*p); i++){
+    h_B[i] = (float)(rand() % 10);
+  }
+  memset(h_C, 0, sizeof(float)*m*p);
+  memset(h_C2, 0, sizeof(float)*m*p);
+
   // start timing
   t0 = clock();
-
-  h_A = new float[DSIZE*DSIZE];
-  h_B = new float[DSIZE*DSIZE];
-  h_C = new float[DSIZE*DSIZE];
-  for (int i = 0; i < DSIZE*DSIZE; i++){
-    h_A[i] = A_val;
-    h_B[i] = B_val;
-    h_C[i] = 0;}
-
-  // Initialization timing
+  mmul_host(h_A, h_B, h_C, m, n, p);
   t1 = clock();
   t1sum = ((double)(t1-t0))/CLOCKS_PER_SEC;
-  printf("Init took %f seconds.  Begin compute\n", t1sum);
+  printf("CPU took %f seconds.\n", t1sum);
 
   // Allocate device memory and copy input data over to GPU
-  cudaMalloc(&d_A, DSIZE*DSIZE*sizeof(float));
-  cudaMalloc(&d_B, DSIZE*DSIZE*sizeof(float));
-  cudaMalloc(&d_C, DSIZE*DSIZE*sizeof(float));
+  cudaMalloc(&d_A, m*n*sizeof(float));
+  cudaMalloc(&d_B, n*p*sizeof(float));
+  cudaMalloc(&d_C, m*p*sizeof(float));
   cudaCheckErrors("cudaMalloc failure");
-  cudaMemcpy(d_A, h_A, DSIZE*DSIZE*sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_B, h_B, DSIZE*DSIZE*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_A, h_A, m*n*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_B, h_B, n*p*sizeof(float), cudaMemcpyHostToDevice);
   cudaCheckErrors("cudaMemcpy H2D failure");
 
   // Cuda processing sequence step 1 is complete
 
   // Launch kernel
   dim3 block(block_size, block_size);  // dim3 variable holds 3 dimensions
-  dim3 grid((DSIZE+block.x-1)/block.x, (DSIZE+block.y-1)/block.y);
-  mmul<<<grid, block>>>(d_A, d_B, d_C, DSIZE);
+  dim3 grid((p+block.x-1)/block.x, (m+block.y-1)/block.y);
+  cudaMemset(d_C, 0, m * p * sizeof(float));
+  mmul<<<grid, block>>>(d_A, d_B, d_C, m, n, p);
+  cudaDeviceSynchronize();
   cudaCheckErrors("kernel launch failure");
 
   // Cuda processing sequence step 2 is complete
 
   // Copy results back to host
-  cudaMemcpy(h_C, d_C, DSIZE*DSIZE*sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_C2, d_C, m*p*sizeof(float), cudaMemcpyDeviceToHost);
 
   // GPU timing
   t2 = clock();
   t2sum = ((double)(t2-t1))/CLOCKS_PER_SEC;
-  printf ("Done. Compute took %f seconds\n", t2sum);
+  printf ("GPU took %f seconds\n", t2sum);
 
   // Cuda processing sequence step 3 is complete
 
   // Verify results
   cudaCheckErrors("kernel execution failure or cudaMemcpy H2D failure");
-  for (int i = 0; i < DSIZE*DSIZE; i++) if (h_C[i] != A_val*B_val*DSIZE) {printf("mismatch at index %d, was: %f, should be: %f\n", i, h_C[i], A_val*B_val*DSIZE); return -1;}
-  printf("Success!\n"); 
+  for (int i = 0; i < m*p; i++) {
+    if (fabs(h_C[i] - h_C2[i]) > EPS) {
+        printf("Mismatch at index %d, was: %f, should be: %f\n", i, h_C[i], h_C2[i]);
+        return -1;
+    }
+  }
+  printf("Success!\n");
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
+
+  delete[] h_A;
+  delete[] h_B;
+  delete[] h_C;
+  delete[] h_C2;
+
   return 0;
 }
   
--- exercises/hw2/readme.md
+++ exercises/hw2/readme.md
@@ -21,14 +21,115 @@ If you have trouble, you can look at *stencil_1d_solution* for a complete exampl
 
 ## **2. 2D Matrix Multiply Using Shared Memory**
 
-Next, let's apply shared memory to the 2D matrix multiply we wrote in Homework 1. FIXME locations are provided in the code skeleton in *matrix_mul_shared.cu*. See if you can successfully load the required data into shared memory and then appropriately update the dot product calculation. Compile and run your code using the following:
-
-```
-module load cuda
-nvcc -o matrix_mul matrix_mul_shared.cu
-lsfrun ./matrix_mul
-```
+Next, let`s apply shared memory to the 2D matrix multiply we wrote in Homework 1. FIXME locations are provided in the code skeleton in *matrix_mul_shared.cu*. See if you can successfully load the required data into shared memory and then appropriately update the dot product calculation. 
 
 Note that timing information is included. Go back and run your solution from Homework 1 and observe the runtime. What runtime impact do you notice after applying shared memory to this 2D matrix multiply? How does it differ from the runtime you observed in your previous implementation?
 
 If you have trouble, you can look at *matrix_mul_shared_solution* for a complete example.
+
+Execute:
+``` bash
+nvcc -o matrix_mul_shared matrix_mul_shared.cu
+.\matrix_mul_shared.exe <m> <n> <p>
+```
+Example:
+``` bash
+.\matrix_mul_shared.exe 512 1024 2048
+```
+
+Sample output:
+``` bash
+512 by 1024 multiply 1024 by 2048
+CPU took 2.828000 seconds.
+GPU took 0.101000 seconds
+Success!
+```
+
+### Important notes on `__syncthreads()` and shared memory usage
+* `__syncthreads()` is a barrier synchronization that **requires all threads in a block to reach the call before any can proceed**. If some threads skip `__syncthreads()` due to conditional statements, it **causes undefined behavior or deadlocks.**
+  
+* When implementing tiled matrix multiplication with shared memory:
+1. Avoid divergent control flow (like if/else/break/continue) around __syncthreads().
+2. Prefer unified control flow per warp/block so all threads execute __syncthreads() the same number of times.
+
+* To handle boundary conditions (when thread indices exceed matrix dimensions), perform boundary checks only when reading from global memory into shared memory and store zeros in shared memory for threads outside bounds.
+
+Use the following main() function to randomly stress test your kernel over multiple iterations.
+``` cpp
+int main(){
+  srand(time(0));
+
+  int m, n, p;
+  int count = 0;
+  while(count < 100){
+    m = (int)(rand() % DSIZE +1);
+    n = (int)(rand() % DSIZE +1);
+    p = (int)(rand() % DSIZE +1);
+    printf("%d time: ", count+1);
+
+    printf("%d by %d multiply %d by %d\n", m, n, n, p);
+
+    float *h_A, *h_B, *h_C, *h_C2, *d_A, *d_B, *d_C;
+
+    // these are just for timing
+    clock_t t0, t1, t2;
+    double t1sum=0.0;
+    double t2sum=0.0;
+
+    // initialize
+    h_A = new float[m*n]; // m x n
+    h_B = new float[n*p]; // n x p
+    h_C = new float[m*p]; // m x p
+    h_C2 = new float[m*p];
+    for (int i = 0; i < (m*n); i++){
+      h_A[i] = (float)(rand() % 10);
+    }
+    for (int i = 0; i < (n*p); i++){
+      h_B[i] = (float)(rand() % 10);
+    }
+    memset(h_C, 0, sizeof(float)*m*p);
+    memset(h_C2, 0, sizeof(float)*m*p);
+    
+    mmul_host(h_A, h_B, h_C, m, n, p);
+
+    // Allocate device memory and copy input data over to GPU
+    cudaMalloc(&d_A, m*n*sizeof(float));
+    cudaMalloc(&d_B, n*p*sizeof(float));
+    cudaMalloc(&d_C, m*p*sizeof(float));
+    cudaCheckErrors("cudaMalloc failure");
+    cudaMemcpy(d_A, h_A, m*n*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, n*p*sizeof(float), cudaMemcpyHostToDevice);
+    cudaCheckErrors("cudaMemcpy H2D failure");
+
+    dim3 block(block_size, block_size);
+    dim3 grid((p+block.x-1)/block.x, (m+block.y-1)/block.y);
+    cudaMemset(d_C, 0, m * p * sizeof(float));
+    mmul2<<<grid, block>>>(d_A, d_B, d_C, m, n, p);
+    cudaDeviceSynchronize();
+    cudaCheckErrors("kernel launch failure");
+
+    cudaMemcpy(h_C2, d_C, m*p*sizeof(float), cudaMemcpyDeviceToHost);
+    
+
+    // Verify results
+    cudaCheckErrors("kernel execution failure or cudaMemcpy H2D failure");
+    for (int i = 0; i < m*p; i++) {
+      if (fabs(h_C[i] - h_C2[i]) > EPS) {
+          printf("Mismatch at index %d, was: %f, should be: %f\n", i, h_C[i], h_C2[i]);
+          return -1;
+      }
+    }
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    delete[] h_A;
+    delete[] h_B;
+    delete[] h_C;
+    delete[] h_C2;
+    count++;
+  }
+  printf("Success!\n");
+  return 0;
+}
+```
